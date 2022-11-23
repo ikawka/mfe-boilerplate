37a30e15be0108665df27d654827fd7869a0b666
ikawka <pixelazion@gmail.com>
2022-11-23 09:50:25 +0800

    update apps

--- container/modulefederation.config.js
+++ container/modulefederation.config.js
@@ -4,8 +4,9 @@ const { name, apps, entryPoint } = require('./manifest.json');
 module.exports = {
   name,
   remotes: Object.keys(apps).reduce((acc, item) => {
-    const domain =
-      process.env.PRODUCTION_DOMAIN || `http://localhost:${apps[item]}`;
+    const domain = process.env.PRODUCTION_DOMAIN
+      ? `${process.env.PRODUCTION_DOMAIN}/${item}/latest`
+      : `http://localhost:${apps[item]}`;
     acc[item] = `${item}@${domain}/remoteEntry.js`;
     return acc;
   }, {}),
