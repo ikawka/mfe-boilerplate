2f0b3c5df10c288d13b49425cee1bb22c2c50bb7
ikawka <pixelazion@gmail.com>
2022-11-24 13:54:18 +0800

    update container

--- container/src/pages/marketing.tsx
+++ container/src/pages/marketing.tsx
@@ -1,28 +1,8 @@
 import { mount } from 'marketing/App';
-import { useRef, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import RemoteApp from '../components/remote-app';
 
 const Marketing = ({ colorScheme }: any) => {
-  const ref = useRef(null);
-  const history = useHistory();
-
-  useEffect(() => {
-    if (ref.current) {
-      const { onParentNavigate } = mount(ref.current, {
-        initialPath: history.location.pathname,
-        colorScheme,
-        onNavigate: ({ pathname: nextPathname }: any) => {
-          const { pathname } = history.location;
-          if (pathname !== nextPathname) {
-            history.push(nextPathname);
-          }
-        },
-      });
-      history.listen(onParentNavigate);
-    }
-  }, [ref.current]);
-
-  return <div ref={ref} />;
+  return <RemoteApp colorScheme={colorScheme} mount={mount} />;
 };
 
 export default Marketing;
